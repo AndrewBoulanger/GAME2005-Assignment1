4ad9771e8fedfc1c7f1016d724767f8dba80e31f
Andrew Boulanger <andrew_boulanger14@hotmail.com>
2020-10-05 16:59:27 -0400

    Adding name labels to start scene (also the git ingore changes did not work)

--- src/StartScene.cpp
+++ src/StartScene.cpp
@@ -46,11 +46,19 @@ void StartScene::handleEvents()
 void StartScene::start()
 {
 	const SDL_Color blue = { 0, 0, 255, 255 };
-	m_pStartLabel = new Label("START SCENE", "Consolas", 80, blue, glm::vec2(400.0f, 40.0f));
+	m_pStartLabel = new Label("GAME2005 Assignment 1 ", "Consolas", 60, blue, glm::vec2(400.0f, 40.0f));
 	m_pStartLabel->setParent(this);
 	addChild(m_pStartLabel);
 
-	m_pInstructionsLabel = new Label("Press 1 to Play", "Consolas", 40, blue, glm::vec2(400.0f, 120.0f));
+	m_pNameLabel1 = new Label("Andrew Boulanger - 101 292 574", "Consolas", 32, blue, glm::vec2(400.0f, 120.0f));
+	m_pNameLabel1->setParent(this);
+	addChild(m_pNameLabel1);
+
+	m_pNameLabel2 = new Label("Robert Palermo - ", "Consolas", 32, blue, glm::vec2(400.0f, 180.0f));
+	m_pNameLabel2->setParent(this);
+	addChild(m_pNameLabel2);
+
+	m_pInstructionsLabel = new Label("press start button to Play", "Consolas", 40, blue, glm::vec2(400.0f, 500.0f));
 	m_pInstructionsLabel->setParent(this);
 	addChild(m_pInstructionsLabel);
 
